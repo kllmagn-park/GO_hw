01cdc8a6df2ef8dde946c5ce69e01e7881fe4dcb
Роман Медников <jellybe@yandex.ru>
2022-02-24 14:14:26 +0300

    Увеличено количество тестов для uniq.

--- uniq/uniq/uniq_test.go
+++ uniq/uniq/uniq_test.go
@@ -5,7 +5,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestUniq(t *testing.T) {
+func TestNoOptions(t *testing.T) {
 	var lines []string
 	var err error
 	var options Options
@@ -24,6 +24,13 @@ func TestUniq(t *testing.T) {
 			"I love music",
 		}, "they should be equal")
 	}
+}
+
+func TestIgnoreCase(t *testing.T) {
+	var lines []string
+	var err error
+	var options Options
+	optionsDefault := GetDefaultOptions()
 
 	options = optionsDefault
 	options.IgnoreCase = true
@@ -37,6 +44,153 @@ func TestUniq(t *testing.T) {
 		assert.Equal(t, lines,
 		[]string {
 			"I LOVE MUSIC",
+		}, "they should be equal")
+	}
+}
+
+func TestFieldsChars(t *testing.T) {
+	var lines []string
+	var err error
+	var options Options
+	optionsDefault := GetDefaultOptions()
+
+	options = optionsDefault
+	options.NumFields = 1
+	lines, err = Uniq(
+		[]string {
+			"I love music",
+			"I love music",
+			" ",
+			"I LOVE MUSIC",
+		}, options,
+	)
+	if assert.Nil(t, err) {
+		assert.Equal(t, lines,
+		[]string {
+			"I love music",
+			"I love music",
+			" ",
+			"I LOVE MUSIC",
+		}, "they should be equal")
+	}
+
+	options = optionsDefault
+	options.NumFields = 1
+	options.NumChars = 1
+	lines, err = Uniq(
+		[]string {
+			"A love music",
+			"B love music",
+			" ",
+			"C I LOVE MUSIC",
+			"D I LOVE MUSIC",
+		}, options,
+	)
+	if assert.Nil(t, err) {
+		assert.Equal(t, lines,
+		[]string {
+			"A love music",
+			"B love music",
+			" ",
+			"C I LOVE MUSIC",
+		}, "they should be equal")
+	}
+}
+
+func TestOutputUniq(t *testing.T) {
+	var lines []string
+	var err error
+	var options Options
+	optionsDefault := GetDefaultOptions()
+
+	options = optionsDefault
+	options.OutputUnique = true
+	lines, err = Uniq(
+		[]string {
+			"I love music",
+			"I do not love music",
+			"I love music",
+			"I LOVE MUSIC",
+		}, options,
+	)
+	if assert.Nil(t, err) {
+		assert.Equal(t, lines,
+		[]string {
+			"I do not love music",
+			"I LOVE MUSIC",
+		}, "they should be equal")
+	}
+}
+
+func TestOutputRepeated(t *testing.T) {
+	var lines []string
+	var err error
+	var options Options
+	optionsDefault := GetDefaultOptions()
+
+	options = optionsDefault
+	options.OutputRepeated = true
+	lines, err = Uniq(
+		[]string {
+			"I love music",
+			"I do not love music",
+			"I love music",
+			"I LOVE MUSIC",
+		}, options,
+	)
+	if assert.Nil(t, err) {
+		assert.Equal(t, lines,
+		[]string {
+			"I love music",
+			"I love music",
+		}, "they should be equal")
+	}
+}
+
+func TestCount(t *testing.T) {
+	var lines []string
+	var err error
+	var options Options
+	optionsDefault := GetDefaultOptions()
+
+	options = optionsDefault
+	options.UseCount = true
+	lines, err = Uniq(
+		[]string {
+			"I love music",
+			"I do not love music",
+			"I love music",
+			"I LOVE MUSIC",
+		}, options,
+	)
+	if assert.Nil(t, err) {
+		assert.Equal(t, lines,
+		[]string {
+			"2 I love music",
+			"1 I do not love music",
+			"2 I love music",
+			"1 I LOVE MUSIC",
+		}, "they should be equal")
+	}
+
+	options = optionsDefault
+	options.UseCount = true
+	options.IgnoreCase = true
+	lines, err = Uniq(
+		[]string {
+			"I love music",
+			"I do not love music",
+			"I love music",
+			"I LOVE MUSIC",
+		}, options,
+	)
+	if assert.Nil(t, err) {
+		assert.Equal(t, lines,
+		[]string {
+			"3 I love music",
+			"1 I do not love music",
+			"3 I love music",
+			"3 I LOVE MUSIC",
 		}, "they should be equal")
 	}
 }
